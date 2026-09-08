b9d32008d7cac72198e164857a08f7ded1f35c8c
John Miller <johnmiller.09@yahoo.com>
2026-09-08 09:51:19 -0400

    Add automated AWS deployment pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -342,3 +342,96 @@ resource "aws_lb_listener" "http" {
 output "dashboard_url" {
   value = "http://${aws_lb.app.dns_name}"
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "cloudops-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:johnmiller-lovescode/cloudops-status-dashboard:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Project = "cloudops-dashboard"
+  }
+}
+
+resource "aws_iam_policy" "github_actions_deploy" {
+  name = "cloudops-github-actions-deploy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = aws_ecr_repository.app.arn
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_deploy" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_deploy.arn
+}
